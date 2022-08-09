257f843a0751287702f7ecdd5a2b75907362d614
emeraldhycient <igwezehycient86@gmail.com>
2022-08-09 23:40:32 +0100

    [update] added few checks for whatsapp message

--- src/routes/Webhook.js
+++ src/routes/Webhook.js
@@ -7,7 +7,7 @@ const Whatsapp = new WhatsappCloudAPI({
   WABA_ID: process.env.Meta_WA_businessId,
 });
 
-router.get("/webhook", (req, res) => {
+router.get("/webhook", async (req, res) => {
   try {
     console.log("GET: Someone is pinging me!");
 
@@ -31,44 +31,41 @@ router.get("/webhook", (req, res) => {
   }
 });
 
-router.post("/webhook", (req, res) => {
+router.post("/webhook", async (req, res) => {
   try {
     console.log("POST: Someone is pinging me!");
     let data = Whatsapp.parseMessage(req.body);
 
-        if (data?.isMessage) {
-            let incomingMessage = data.message;
-            let recipientPhone = incomingMessage.from.phone; // extract the phone number of sender
-            let recipientName = incomingMessage.from.name;
-            let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
-            let message_id = incomingMessage.message_id; // extract the message id
+    if (data?.isMessage) {
+      let incomingMessage = data.message;
+      let recipientPhone = incomingMessage.from.phone; // extract the phone number of sender
+      let recipientName = incomingMessage.from.name;
+      let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
+      let message_id = incomingMessage.message_id; // extract the message id
 
-            if (typeOfMsg === 'text_message') {
-    await Whatsapp.sendSimpleButtons({
-        message: `Hey ${recipientName}, \nYou are speaking to a chatbot.\nWhat do you want to do next?`,
-        recipientPhone: recipientPhone, 
-        listOfButtons: [
+      if (typeOfMsg === "text_message") {
+        await Whatsapp.sendSimpleButtons({
+          message: `Hey ${recipientName}, \nYou are speaking to a chatbot.\nWhat do you want to do next?`,
+          recipientPhone: recipientPhone,
+          listOfButtons: [
             {
-                title: 'swift dictionary',
-                id: 'swift_dictionary',
+              title: "swift dictionary",
+              id: "swift_dictionary",
             },
             {
-                title: 'Swift summary',
-                id: 'swift_summary',
+              title: "Swift summary",
+              id: "swift_summary",
             },
             {
-              title: 'Swift Urban Dictionary',
-              id: 'swift_urban_dictionary',
-          },
-         
-        ],
-    });
-}
-        }
+              title: "Swift Urban Dictionary",
+              id: "swift_urban_dictionary",
+            },
+          ],
+        });
+      }
+    }
 
-        return res.sendStatus(200);
-
-
+    return res.sendStatus(200);
   } catch (error) {
     console.error({ error });
     return res.sendStatus(500);
